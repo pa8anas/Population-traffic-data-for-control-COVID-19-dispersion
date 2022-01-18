c91e52effd79626dd3f18426d863175ca195e046
mamoueli <75751850+mamoueli@users.noreply.github.com>
2022-01-18 22:36:44 +0200

    Add files via upload

---
+++ infectedUsers.sql
@@ -0,0 +1,21 @@
+-- infectedusers table, active infected users
+
+
+ CREATE TABLE `infectedusers` (
+  `date` date NOT NULL,
+  `userID` varchar(255) NOT NULL,
+  `infected` tinyint(1) NOT NULL,
+  PRIMARY KEY (`userID`),
+  CONSTRAINT `infectedusers_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 |
+
+
+
+--Event for deleting entries after 14 days
+
+CREATE EVENT IF NOT EXISTS `Delete_log_after_14days`
+ON SCHEDULE
+  EVERY 1 DAY
+  DO
+DELETE FROM infectedusers
+    WHERE date < DATE_SUB(NOW(), INTERVAL 14 DAY)
---
+++ userHistory.sql
@@ -0,0 +1,10 @@
+-- userhistory, logs remain after 14 days 
+
+
+ CREATE TABLE `userhistory` (
+  `userid` varchar(1000) NOT NULL,
+  `date` date DEFAULT NULL,
+  `infected` tinyint(1) DEFAULT NULL,
+  KEY `userid` (`userid`),
+  CONSTRAINT `userhistory_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 |
