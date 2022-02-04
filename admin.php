0df278767ca0fed1e485d0ee7ce8e9c44b8ccd7f
mamoueli <75751850+mamoueli@users.noreply.github.com>
2022-02-04 19:19:09 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['loggedin'])) {
 <head>
     <meta charset="utf-8">
     <title>Admin Dashboard</title>
-    <link href="style_.css" rel="stylesheet" type="text/css">
+    <link href="style.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 </head>
 <body class="loggedin">
